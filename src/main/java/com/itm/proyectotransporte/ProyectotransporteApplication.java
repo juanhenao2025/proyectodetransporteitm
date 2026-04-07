8c1097863fdc21b5728ec5bcbfc401596982cab9
Juan David Henao <henaogallegoj017@gmail.com>
2026-04-07 15:30:51 -0500

    feat: estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/itm/proyectotransporte/ProyectotransporteApplication.java
@@ -0,0 +1,13 @@
+package com.itm.proyectotransporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectotransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectotransporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itm/proyectotransporte/ProyectotransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itm.proyectotransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectotransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
